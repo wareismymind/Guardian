13b0a4777acf6d59c65a880a5808b1c760189e24
Thomas Showers <showerstm@gmail.com>
2017-11-27 18:49:15 -0400

    Add range valdations for IComparable arguments.
    Adds range IsLessThan, IsNotLessThan, IsGreaterThan, and
    IsNotGreaterThan validations to IComparable arguments.

---
+++ wimm.Guardian.UnitTests/ArgumentRangeExtensionsTests.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Moq;
+using Xunit;
+
+namespace wimm.Guardian.UnitTests
+{
+    public class ArgumentRangeExtensionsTests
+    {
+        [Fact(DisplayName = "IsLessThan throws for null target")]
+        public void IsLessThan_Throws_For_Null_Target()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as Argument<int>).IsLessThan(0));
+        }
+
+        [Fact(DisplayName = "IsLessThan throws for null value")]
+        public void IsLessThan_Throws_For_Null_Value()
+        {
+            var subject = new Mock<ISubject<string>>();
+            subject.SetupGet(s => s.Value).Returns(null as string);
+            var argument = new Argument<string>(subject.Object);
+            Assert.Throws<ArgumentNullException>(() => argument.IsLessThan(null as string));
+        }
+
+        [Fact(DisplayName = "IsLessThan does not throw when less than.")]
+        public void IsLessThan_Does_Not_Throw_When_Less_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            argument.IsLessThan(1);
+        }
+
+        [Fact(DisplayName = "IsLessThan throws when equal")]
+        public void IsLessThan_Throws_When_Equal()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            Assert.Throws<ArgumentOutOfRangeException>(() => argument.IsLessThan(0));
+        }
+
+        [Fact(DisplayName = "IsLessThan throws when greater than.")]
+        public void IsLessThan_Throws_When_Greater_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(1);
+            var argument = new Argument<int>(subject.Object);
+            Assert.Throws<ArgumentOutOfRangeException>(() => argument.IsLessThan(0));
+        }
+
+        [Fact(DisplayName = "IsNotLessThan throws for null target")]
+        public void IsNotLessThan_Throws_For_Null_Target()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as Argument<int>).IsNotLessThan(0));
+        }
+
+        [Fact(DisplayName = "IsNotLessThan throws for null value")]
+        public void IsNotLessThan_Throws_For_Null_Value()
+        {
+            var subject = new Mock<ISubject<string>>();
+            subject.SetupGet(s => s.Value).Returns(null as string);
+            var argument = new Argument<string>(subject.Object);
+            Assert.Throws<ArgumentNullException>(() => argument.IsNotLessThan(null as string));
+        }
+
+        [Fact(DisplayName = "IsNotLessThan throws when less than.")]
+        public void IsNotLessThan_Throws_When_Less_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            Assert.Throws<ArgumentOutOfRangeException>(() => argument.IsNotLessThan(1));
+        }
+
+        [Fact(DisplayName = "IsNotLessThan does not throw when equal.")]
+        public void IsNotLessThan_Does_Not_Throw_When_Equal()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            argument.IsNotLessThan(0);
+        }
+
+        [Fact(DisplayName = "IsNotLessThan does not throw when greater than.")]
+        public void IsNotLessThan_Does_Not_Throw_When_Greater_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(1);
+            var argument = new Argument<int>(subject.Object);
+            argument.IsNotLessThan(0);
+        }
+
+        [Fact(DisplayName = "IsGreaterThan throws for null target")]
+        public void IsGreaterThan_Throws_For_Null_Target()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as Argument<int>).IsGreaterThan(0));
+        }
+
+        [Fact(DisplayName = "IsGreaterThan throws for null value")]
+        public void IsGreaterThan_Throws_For_Null_Value()
+        {
+            var subject = new Mock<ISubject<string>>();
+            subject.SetupGet(s => s.Value).Returns(null as string);
+            var argument = new Argument<string>(subject.Object);
+            Assert.Throws<ArgumentNullException>(() => argument.IsGreaterThan(null as string));
+        }
+
+        [Fact(DisplayName = "IsGreaterThan throws when less than.")]
+        public void IsGreaterThan_Throws_When_Less_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            Assert.Throws<ArgumentOutOfRangeException>(() => argument.IsGreaterThan(1));
+        }
+
+        [Fact(DisplayName = "IsGreaterThan throws when equal.")]
+        public void IsGreaterThan_Throws_When_Equal()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            Assert.Throws<ArgumentOutOfRangeException>(() => argument.IsGreaterThan(0));
+        }
+
+        [Fact(DisplayName = "IsGreaterThan does not throw when greater than.")]
+        public void IsGreaterThan_Does_Not_Throw_When_Greater_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(1);
+            var argument = new Argument<int>(subject.Object);
+             argument.IsGreaterThan(0);
+        }
+
+        [Fact(DisplayName = "IsNotGreaterThan throws for null target")]
+        public void IsNotGreaterThan_Throws_For_Null_Target()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => (null as Argument<int>).IsNotGreaterThan(0));
+        }
+
+        [Fact(DisplayName = "IsNotGreaterThan throws for null value")]
+        public void IsNotGreaterThan_Throws_For_Null_Value()
+        {
+            var subject = new Mock<ISubject<string>>();
+            subject.SetupGet(s => s.Value).Returns(null as string);
+            var argument = new Argument<string>(subject.Object);
+            Assert.Throws<ArgumentNullException>(() => argument.IsNotGreaterThan(null as string));
+        }
+
+        [Fact(DisplayName = "IsNotGreaterThan does not throw when less than.")]
+        public void IsNotGreaterThan_Throws_When_Less_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            argument.IsNotGreaterThan(1);
+        }
+
+        [Fact(DisplayName = "IsNotGreaterThan does not throw when equal.")]
+        public void IsNotGreaterThan_Does_Not_Throw_When_Equal()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(0);
+            var argument = new Argument<int>(subject.Object);
+            argument.IsNotGreaterThan(0);
+        }
+
+        [Fact(DisplayName = "IsNotGreaterThan throws when greater than.")]
+        public void IsNotGreaterThan_Does_Not_Throw_When_Greater_Than()
+        {
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Value).Returns(1);
+            var argument = new Argument<int>(subject.Object);
+            Assert.Throws<ArgumentOutOfRangeException>(() => argument.IsNotGreaterThan(0));
+        }
+    }
+}
---
+++ wimm.Guardian/ArgumentRangeExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace wimm.Guardian
+{
+    /// <summary>
+    /// Adds range validations for numeric <see cref="Argument{T}"/> types.
+    /// </summary>
+    public static class ArgumentRangeExtensions
+    {
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if the value of
+        /// <paramref name="target"/> is not less than <paramref name="value"/>.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>The <see cref="Argument{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="target"/>, or <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public static Argument<T> IsLessThan<T, TValue>(
+            this Argument<T> target, TValue value) where T : IComparable<TValue>
+        {
+            target.Require(nameof(value)).Argument().IsNotNull();
+            value.Require(nameof(value)).Argument().IsNotNull();
+
+            return
+                target.IfNot(
+                    v => v.IsLessThan(value),
+                    (a) =>
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            a.Name, $"Must be less than {value}.");
+                    });
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if the value of
+        /// <paramref name="target"/> is less than <paramref name="value"/>.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>The <see cref="Argument{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="target"/>, or <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public static Argument<T> IsNotLessThan<T, TValue>(
+            this Argument<T> target, TValue value) where T : IComparable<TValue>
+        {
+            target.Require(nameof(value)).Argument().IsNotNull();
+            value.Require(nameof(value)).Argument().IsNotNull();
+
+            return
+                target.If(
+                    v => v.IsLessThan(value),
+                    (a) =>
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            a.Name, $"Must not be less than {value}.");
+                    });
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if the value of
+        /// <paramref name="target"/> is not greater than <paramref name="value"/>.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>The <see cref="Argument{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="target"/>, or <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public static Argument<T> IsGreaterThan<T, TValue>(
+            this Argument<T> target, TValue value) where T : IComparable<TValue>
+        {
+            target.Require(nameof(value)).Argument().IsNotNull();
+            value.Require(nameof(value)).Argument().IsNotNull();
+
+            return
+                target.IfNot(
+                    v => v.IsGreaterThan(value),
+                    (a) =>
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            a.Name, $"Must be greater than {value}.");
+                    });
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if the value of
+        /// <paramref name="target"/> is greater than <paramref name="value"/>.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>The <see cref="Argument{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="target"/>, or <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public static Argument<T> IsNotGreaterThan<T, TValue>(
+            this Argument<T> target, TValue value) where T : IComparable<TValue>
+        {
+            target.Require(nameof(value)).Argument().IsNotNull();
+            value.Require(nameof(value)).Argument().IsNotNull();
+
+            return
+                target.If(
+                    v => v.IsGreaterThan(value),
+                    (a) =>
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            a.Name, $"Must not be greater than {value}.");
+                    });
+        }
+
+        private static bool IsLessThan<T>(this IComparable<T> target, T value)
+        {
+            return target.CompareTo(value) < 0;
+        }
+
+        private static bool IsGreaterThan<T>(this IComparable<T> target, T value)
+        {
+            return target.CompareTo(value) > 0;
+        }
+    }
+}
