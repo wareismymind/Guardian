ed3f6dbc7463da53324486cd5cd3934f032a53f8
Thomas Showers <showerstm@gmail.com>
2017-11-27 17:40:03 -0400

    Re-order Argument tests to match class.

--- wimm.Guardian.UnitTests/ArgumentTests.cs
+++ wimm.Guardian.UnitTests/ArgumentTests.cs
@@ -30,27 +30,27 @@ namespace wimm.Guardian.UnitTests
             }
         }
 
-        [Fact(DisplayName = "Argument extension throws for null subject.")]
-        public void Argument_Extension_Throws_For_Null_Subject()
-        {
-            Assert.Throws<ArgumentNullException>(() => (null as ISubject<int>).Argument());
-        }
-
-        [Fact(DisplayName = "Argument extension returns equivalent ISubject")]
-        public void Argument_Extension_Returns_Equialvent_ISubject()
+        [Fact(DisplayName = "IsNotNull throws for null valued Argument")]
+        public void IsNotNull_Throws_For_Null_Valued_Argument()
         {
             var name = "name";
-            var value = 42;
-            var subject = new Mock<ISubject<int>>();
-            subject.SetupGet(s => s.Name).Returns(name);
-            subject.SetupGet(s => s.Value).Returns(value);
-            var argument = subject.Object.Argument();
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => (null as object).Require(name).Argument().IsNotNull());
+            Assert.Equal(name, ex.ParamName);
+        }
 
-            foreach (var propertyInfo in typeof(ISubject<int>).GetProperties())
-            {
-                Assert.Equal(
-                    propertyInfo.GetValue(subject.Object), propertyInfo.GetValue(argument));
-            }
+        [Fact(DisplayName = "IsNotNull does not throw for non-null valued Argument")]
+        public void IsNotNull_Does_Not_Throw_For_NonNull_Valued_Argument()
+        {
+            "value".Require("name").Argument().IsNotNull();
+        }
+
+        [Fact(DisplayName = "IsNotNull returns target")]
+        public void IsNotNull_Returns_Target()
+        {
+            var argument = "value".Require("name").Argument();
+            var returned = argument.IsNotNull();
+            Assert.Same(argument, returned);
         }
 
         [Fact(DisplayName = "If throws for null condition")]
@@ -145,27 +145,27 @@ namespace wimm.Guardian.UnitTests
             Assert.Same(argument, returned);
         }
 
-        [Fact(DisplayName = "IsNotNull throws for null valued Argument")]
-        public void IsNotNull_Throws_For_Null_Valued_Argument()
+        [Fact(DisplayName = "Argument extension throws for null subject.")]
+        public void Argument_Extension_Throws_For_Null_Subject()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as ISubject<int>).Argument());
+        }
+
+        [Fact(DisplayName = "Argument extension returns equivalent ISubject")]
+        public void Argument_Extension_Returns_Equialvent_ISubject()
         {
             var name = "name";
-            var ex = Assert.Throws<ArgumentNullException>(
-                () => (null as object).Require(name).Argument().IsNotNull());
-            Assert.Equal(name, ex.ParamName);
-        }
+            var value = 42;
+            var subject = new Mock<ISubject<int>>();
+            subject.SetupGet(s => s.Name).Returns(name);
+            subject.SetupGet(s => s.Value).Returns(value);
+            var argument = subject.Object.Argument();
 
-        [Fact(DisplayName = "IsNotNull does not throw for non-null valued Argument")]
-        public void IsNotNull_Does_Not_Throw_For_NonNull_Valued_Argument()
-        {
-            "value".Require("name").Argument().IsNotNull();
-        }
-
-        [Fact(DisplayName = "IsNotNull returns target")]
-        public void IsNotNull_Returns_Target()
-        {
-            var argument = "value".Require("name").Argument();
-            var returned = argument.IsNotNull();
-            Assert.Same(argument, returned);
+            foreach (var propertyInfo in typeof(ISubject<int>).GetProperties())
+            {
+                Assert.Equal(
+                    propertyInfo.GetValue(subject.Object), propertyInfo.GetValue(argument));
+            }
         }
     }
 }
