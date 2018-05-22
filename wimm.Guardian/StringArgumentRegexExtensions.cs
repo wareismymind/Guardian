d2fe290068edd18dda1b4ff528ad4415c3caab53
Unknown <Chris.Nantau@gmail.com>
2018-05-22 18:24:54 -0300

    Documentation and test fixes

--- wimm.Guardian/StringArgumentRegexExtensions.cs
+++ wimm.Guardian/StringArgumentRegexExtensions.cs
@@ -8,45 +8,66 @@ namespace wimm.Guardian
     /// </summary>
     public static class StringArgumentRegexExtensions
     {
+
         /// <summary>
-        /// TODO:CN
+        /// Throws an exception if the <see cref="Argument{T}.Value"/> member of 
+        /// <paramref name="pattern"/> does not match the regex <paramref name="pattern"/>
         /// </summary>
-        /// <param name="target"></param>
-        /// <param name="pattern"></param>
-        /// <returns></returns>
+        /// <param name="target"> The target to be evaluated </param>
+        /// <param name="pattern"> A valid .Net regex </param>
+        /// <returns> 
+        /// An <see cref="Argument{T}"/> equivalent to <paramref name="target"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="pattern"/> is <c>null</c>
+        /// </exception>
+        /// <exception cref="ArgumentException"> 
+        /// <paramref name="pattern"/> is not a valid .Net regex 
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// The <see cref="Argument{T}.Value"/> member of <paramref name="target"/> does not match
+        /// the regex given by <paramref name="pattern"/>
+        /// </exception>
         public static Argument<string> IsMatch(this Argument<string> target, string pattern)
         {
             var regexArg = pattern.Require(nameof(pattern)).IsNotNull();
+            Regex reg = null;
 
             try
             {
-                var reg = new Regex(pattern);
-                target.IsMatch(pattern);
-                return target;
+               reg = new Regex(pattern);
             }
             catch (ArgumentException ex)
             {
                 throw new ArgumentException($"{pattern} must be a valid regular expression", regexArg.Name, ex);
             }
+
+            target.IsMatch(reg);
+            return target;
         }
 
         /// <summary>
-        /// TODO:CN
+        /// Throws an exception if the <see cref="Argument{T}.Value"/> member of 
+        /// <paramref name="regex"/> does not match <paramref name="regex"/>
         /// </summary>
-        /// <param name="target"></param>
-        /// <param name="regex"></param>
-        /// <returns></returns>
+        /// <param name="target"> The target to be evaluated </param>
+        /// <param name="regex"> The regex against which <paramref name="target"/> is to be 
+        /// compared </param>
+        /// <returns> 
+        /// An <see cref="Argument{T}"/> equivalent to <paramref name="target"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException"> If</exception>
         /// <exception cref="ArgumentException"></exception>
         public static Argument<string> IsMatch(this Argument<string> target, Regex regex)
         {
+            regex.Require(nameof(regex)).IsNotNull();
             target.IsNotNull();
 
-            if (regex.IsMatch(target.Value))
+            if (!regex.IsMatch(target.Value))
                 throw new ArgumentException($"{target.Name} must match pattern {regex}", target.Name);
 
 
             return target;
         }
-
     }
 }
