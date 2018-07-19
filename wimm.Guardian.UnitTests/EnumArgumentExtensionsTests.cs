187447db63330aef4e7a55c7b29b15991eeeef1a
Unknown <Chris.Nantau@gmail.com>
2018-07-18 22:48:20 -0300

    Added tests for EnumArgumentExtension.IsDefined

---
+++ wimm.Guardian.UnitTests/EnumArgumentExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wimm.Guardian.UnitTests
+{
+    [TestClass]
+    public class EnumArgumentExtensionsTests
+    {
+        [TestMethod]
+        public void IsDefined_ValueUndefined_Throws()
+        {
+            var underTest = new Argument<TestEnum>("Doot", (TestEnum)(int.MaxValue));
+            Assert.ThrowsException<InvalidEnumArgumentException>(() => underTest.IsDefined());
+        }
+
+        [TestMethod]
+        public void IsDefined_ValueNotEnum_Throws()
+        {
+            var underTest = new Argument<DateTime>("Doot", DateTime.Now);
+            Assert.ThrowsException<TypeArgumentException>(() => underTest.IsDefined());
+        }
+
+        [TestMethod]
+        public void IsDefined_ValueDefined_ReturnsSameValue()
+        {
+            var underTest = new Argument<TestEnum>("Doot", TestEnum.Yes);
+            var res = underTest.IsDefined();
+
+            Assert.AreEqual(res.Value, underTest.Value);
+        }
+
+        private enum TestEnum
+        {
+            Yes,
+            No
+        }
+    }
+
+
+}
