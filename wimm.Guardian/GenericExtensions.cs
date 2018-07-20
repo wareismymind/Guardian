98c5f74c62add6c3260ecf6c74c57a877c9c4197
Unknown <Chris.Nantau@gmail.com>
2018-07-20 17:11:40 -0300

    Implementation of HasAttribute

--- wimm.Guardian/GenericExtensions.cs
+++ wimm.Guardian/GenericExtensions.cs
@@ -1,4 +1,11 @@
-﻿namespace wimm.Guardian
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//TODO:I47 -> Remove eventually, or not
+[assembly:InternalsVisibleTo("wimm.Guardian.UnitTests")]
+
+namespace wimm.Guardian
 {
     internal static class GenericExtensions
     {
@@ -6,6 +13,15 @@
         {
             if (default(T) == null)
                 argument.IsNotNull();
+        }
+
+        //TODO:I47 -> Internal for now until tested and doc'd
+        internal static Argument<T> HasAttribute<T>(this Argument<T> argument, Type attributeType)
+        {
+            var attribute = typeof(T).GetTypeInfo().GetCustomAttribute(attributeType)
+                ?? throw new TypeArgumentException(argument.Name, typeof(T));
+
+            return argument;
         }
     }
 }
