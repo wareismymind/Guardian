28ee9d3b3f7e2c8e9311d0c8e0285d991e24dd6e
Insomniak47 <Chris.Nantau@gmail.com>
2018-07-25 10:26:05 -0300

    Feature/enum flag combo (#8)
    * Add Chris Nantau to authors and use master for lic
    
    * Disable package on build
    
    - The build process will handle packaging and versioning so this commit
    disables package on build and resets the committed version info to 0.1.0
    
    * Initial test implementation/spec of IsFlagCombo
    
    * Added message param to TypeArgument for times when you fail for different reasons
    
    * GenericExtension tests for method to extract flags
    
    * Implementation of HasAttribute
    
    * IsFlagCombo cleanup and long tests
    
    * Helper class for popcnting
    
    * Using cleanup
    
    * IsFlagCombo implementation
    
    * Documentation for IsFlagCombo
    
    * Cleanup of BitHelper
    
    * Always the same bloody typo
    
    * IsDefinedEnum now uses IsEnum instead of rolling its own
    
    * Cleanup of the IsFlagCombo method. Simpler check for is flag combo and opened it up to additional types. Added tests for some different backing types as well
    
    * Removed the querying of enum underlying type now that we're using IConvertible. The boxing is less than ideal
    
    * Removed a comment that I was using for navigation. Whoops
    
    * +semver: feature -- Removed single bitness checking in order to allow for combined aliases at the cost of punishing people who make bad enums
    
    * Add Chris Nantau to authors and use master for lic
    
    * Disable package on build
    
    - The build process will handle packaging and versioning so this commit
    disables package on build and resets the committed version info to 0.1.0
    
    * Expect SNK to be at source root
    
    - Modifies the signing property condition to expect to find the SNK file
    in the root of the repo. It wont be on the build server yet but in the
    future the build definition will obtain the SNK from a private location.
    
    * Removing bithelper since it's now unused.
    
    * Initial test implementation/spec of IsFlagCombo
    
    * Added message param to TypeArgument for times when you fail for different reasons
    
    * GenericExtension tests for method to extract flags
    
    * Implementation of HasAttribute
    
    * IsFlagCombo cleanup and long tests
    
    * Helper class for popcnting
    
    * Using cleanup
    
    * IsFlagCombo implementation
    
    * Documentation for IsFlagCombo
    
    * Cleanup of BitHelper
    
    * Always the same bloody typo
    
    * IsDefinedEnum now uses IsEnum instead of rolling its own
    
    * Cleanup of the IsFlagCombo method. Simpler check for is flag combo and opened it up to additional types. Added tests for some different backing types as well
    
    * Removed the querying of enum underlying type now that we're using IConvertible. The boxing is less than ideal
    
    * Removed a comment that I was using for navigation. Whoops
    
    * +semver: feature -- Removed single bitness checking in order to allow for combined aliases at the cost of punishing people who make bad enums
    
    * Removing bithelper since it's now unused.
    
    * removing bithelper tests

---
+++ wimm.Guardian.UnitTests/GenericExtensionTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace wimm.Guardian.UnitTests
+{
+    [TestClass]
+    public class GenericExtensionTest
+    {
+
+        [TestMethod]
+        public void HasAttribute_DoesNotHaveAttribute_Throws()
+        {
+            var underTest = new NoAttribute();
+            Assert.ThrowsException<TypeArgumentException>(() =>
+                underTest.Require(nameof(underTest)).HasAttribute(typeof(SerializableAttribute)));
+        }
+
+        [TestMethod]
+        public void HasAttribute_TypeHasAttribute_ReturnsType()
+        {
+            var underTest = new TestClass();
+            var value = underTest.Require(nameof(underTest)).HasAttribute(typeof(SerializableAttribute));
+            Assert.AreSame(underTest, value.Value);
+        }
+
+        [Serializable]
+        private class TestClass
+        {
+            public int i = 0;
+        }
+
+        private class NoAttribute
+        {
+            public int i = 0;
+        }
+    }
+}
