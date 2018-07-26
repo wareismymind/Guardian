9b8261eecf766008ddd22c009719927974d8be9e
Chris Nantau <Chris.Nantau@gmail.com>
2018-07-25 22:17:14 -0300

    Documented IsEnum<T> (public as of last commit)

--- wimm.Guardian/GenericExtensions.cs
+++ wimm.Guardian/GenericExtensions.cs
@@ -24,7 +24,16 @@ namespace wimm.Guardian
             return argument;
         }
 
-
+        /// <summary>
+        /// Throws a <see cref="TypeArgumentException"/> if the provided type param 
+        /// <typeparamref name="T"/> is not an <see cref="Enum"/>
+        /// </summary>
+        /// <typeparam name="T"> The type to be tested </typeparam>
+        /// <param name="argument"> An argument containing a value of <typeparamref name="T"/></param>
+        /// <returns> a copy of the input <paramref name="argument"/></returns>
+        /// <exception cref="TypeArgumentException"> The <typeparamref name="T"/> is not
+        /// an <see cref="Enum"/>
+        /// </exception>
         public static Argument<T> IsEnum<T>(this Argument<T> argument) where T : struct, IComparable
         {
             if (!typeof(T).GetTypeInfo().IsEnum)
